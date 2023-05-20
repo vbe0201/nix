d2e6d9b6262073180501a1d13c1c98454cec792b
Valentin B <valentin.be@protonmail.com>
2023-05-20 22:24:44 +0200

    Add basic glacier config

---
+++ systems/glacier.nix
@@ -0,0 +1,58 @@
+{ config, modulesPath, pkgs, unstable-pkgs, ...  }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    tmpOnTmpfs = true;
+    tmpOnTmpfsSize = "75%";
+
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [];
+
+    kernelModules = ["kvm-amd"];
+    extraModulePackages = [];
+
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/182bb213-c166-4288-8065-8d20378acf88";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/DB14-A397";
+    fsType = "vfat";
+  };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/7edab4b6-ed9a-482a-8ab8-d9476342fea0";
+    fsType = "ext4";
+  };
+
+  swapDevices = [];
+
+  networking = {
+    hostName = "glacier";
+
+    useDHCP = false;
+    interfaces.wlp35s0.useDHCP = true;
+
+    networkmanager.enable = true;
+  };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+}
