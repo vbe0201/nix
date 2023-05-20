cfa739741b2c5cad74f8297940ff3f4cabb3b826
Valentin B <valentin.be@protonmail.com>
2023-05-20 19:11:53 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Configurations for my NixOS system";
+
+  inputs = {
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self inputs; } {
+      imports = [];
+
+      systems = ["x86_64-linux"];
+    };
+}
