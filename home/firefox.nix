687b221b9543e9b212461f4c1478187382f15aa8
Valentin B <valentin.be@protonmail.com>
2023-06-18 02:22:56 +0200

    Add more Firefox settings

--- home/firefox.nix
+++ home/firefox.nix
@@ -20,6 +20,13 @@
           };
           settings = {
             "browser.toolbars.bookmarks.visibility" = "newtab";
+
+            "general.autoScroll" = true;
+
+            "security.webauth.u2f" = true;
+            "security.webauth.webauthn" = true;
+            "security.webauth.webauthn_enable_softtoken" = true;
+            "security.webauth.webauthn_enable_usbtoken" = true;
           };
         };
       };
