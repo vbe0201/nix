4e7024f0fb2d3b670df8136ddc44ac29ed09644c
Valentin B <valentin.be@protonmail.com>
2023-06-22 01:15:23 +0200

    Install protontricks

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,7 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    protontricks
+  ];
 }
