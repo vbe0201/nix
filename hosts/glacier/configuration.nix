581236ba53dc13bfef34a5147c46250591e48eee
Valentin B. <valentin.be@protonmail.com>
2025-10-05 17:04:53 +0200

    Update flake.lock and switch Glacier to more recent NVIDIA

--- hosts/glacier/configuration.nix
+++ hosts/glacier/configuration.nix
@@ -1,6 +1,7 @@
 {
   config,
   modulesPath,
+  pkgs,
   ...
 }: {
   imports = [
@@ -102,5 +103,14 @@
     yubikey.enable = true;
 
     zsh.enable = true;
+  };
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.95.05";
+    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+    openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+    settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+    persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
   };
 }
