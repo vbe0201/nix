8d618cb1d4e804343b6a5945ccdbe5a6e9e86eb2
Valentin B. <valentin.be@protonmail.com>
2026-01-17 00:52:36 +0100

    Switch nixpkgs flake inputs to github syntax

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Personal NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
